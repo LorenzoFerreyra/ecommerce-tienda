c0c01b462c1e340b39ac723a8d253b99ca6e7862
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2024-03-04 09:06:27 -0300

    actualizacion de urls.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -10,3 +10,11 @@ urlpatterns = [
     path('payment/', include('payment.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+"""
+settings.MEDIA_URL es el prefijo que se usa para construir las rutas a los archivos multimedia (media files) en el servidor.
+Si settings.MEDIA_URL es /media/, entonces un archivo con la ruta /media/images/producto.jpg sería accesible a través de la URL /media/images/producto.jpg en el navegador.
+Por otro lado, settings.MEDIA_ROOT es la ubicación física en el sistema de archivos donde se almacenan los archivos de medios. Django sirve estos archivos estáticos automáticamente cuando DEBUG es True en settings.py.
+
+
+"""
