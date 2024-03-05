7645b50ffb36c03fd65216c4a240e773214aacb0
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2024-03-05 12:04:04 -0300

    Opciones

--- store/models.py
+++ store/models.py
@@ -97,7 +97,7 @@ class Order(models.Model):
         ('cash', 'Efectivo'),
         ('card', 'Tarjeta de crédito'),
         ('transfer', 'Transferencia bancaria'),
-        # Agrega más opciones aquí si lo deseas
+        
     ]
     payment_method = models.CharField(max_length=20, choices=PAYMENT_CHOICES, default="cash")
 
