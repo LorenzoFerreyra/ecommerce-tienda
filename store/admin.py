59a754a69a8dc5ab94536e1dfc3f87e2d0d0b25c
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2024-03-05 16:45:18 -0300

    Admin.py new changes

--- store/admin.py
+++ store/admin.py
@@ -6,22 +6,32 @@ from django.contrib.auth.models import User
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Order)
 admin.site.register(Profile)
-admin.site.register(OrderItem)
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'created', 'updated', 'paid']
+    list_filter = ['paid', 'created', 'updated']
+    search_fields = ['id', 'first_name', 'last_name', 'email']
+    inlines = [OrderItemInline]
+
+# admin.site.register(OrderItem) # Esta línea también puede ser eliminada
 
 # Mostrar User y Profile en la misma página que la información del usuario.
 class ProfileInline(admin.StackedInline):
-	model = Profile
+    model = Profile
 
 # el objetivo es que cuando se vea o edite un User en el panel de administración,
 # también se mostrará y se podrá editar su perfil en la misma página.
 class UserAdmin(admin.ModelAdmin):
-	model = User
-	field = ["username", "first_name", "last_name", "email"]
-	inlines = [ProfileInline]
+    model = User
+    field = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
 
 admin.site.unregister(User)
 
 admin.site.register(User, UserAdmin)
-
